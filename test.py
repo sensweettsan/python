abaf5d5218913f94ff9125c22038e9932bece34d
samuel <samuelfsp72@gmail.com>
2024-06-07 21:22:49 -0300

    criação de arquivos de exemplo

---
+++ test.py
@@ -0,0 +1,20 @@
+print('olá, mundo')
+
+print(5+2)
+
+print(6-4)
+
+print(5*4)
+
+print(10/4)
+
+print(10//4)
+
+print(5%2)
+
+print(2**3)
+
+print(10%4/6+3)
+
+
+
---
+++ variaveis.py
@@ -0,0 +1,12 @@
+'''
+data:07/06/2024
+Desenvolvido por Samuel
+'''
+
+#variavel
+nota1 = float(input('digite a note 1:'))
+nota2 = float(input('digite a note 2:'))
+media = (nota1 + nota2)/2
+
+print('Média: ', media)
+
